c24c0010e9d836c1865a77b46362f294be82601e
Daniel Laforge <daniel.laforge5@gmail.com>
2023-02-14 08:02:10 -0400

    Quiz questions in JS file

---
+++ Assets/questions.js
@@ -0,0 +1,29 @@
+// Questions were taken from W3 Schools tutorial quiz.
+
+var questionBank = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        selection: ["<javascript>", "<scripting>", "<js>", "<script>"],
+        answer: "<script>"
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        selection: ["var colors = ["red", "green", "blue"]", "var colors = (1:"red", 2:"green", 3:"blue")", "var colors = "red", "green", "blue"", "var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")"],
+        answer: "var colors = ["red", "green", "blue"]"
+    },
+    {
+        question: "What does CSS stand for?",
+        selection: ["Colorful Style Sheets", "Cascading Style Sheets", "Creative Style Sheets", "Computer Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: "Choose the correct HTML element for the largest heading:",
+        selection: ["<heading>", "<head>", "<h1>", "<h6>"],
+        answer: "<h1>"
+    },
+    {
+        question: "In HTML, which attribute is used to specify that an input field must be filled out?",
+        selection: ["placeholder", "formvalidate", "validate", "required"],
+        answer: "required"
+    },
+];
