c7d9ec4c4506b29b93809759870f6b7c309fb073
Daniel Laforge <daniel.laforge5@gmail.com>
2023-02-15 11:18:18 -0400

    javascript and some css

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,6 @@
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
+var evaluateAnswer = document.getElementById("#evaluateAnswer")
 var timeLeft = 120;
 var timeInterval;
 var startBtn = document.getElementById("startBtn")
@@ -31,17 +32,37 @@ function countdown() {
 function displayQuestion() {
   var questionEl = document.getElementById("question")
   questionEl.textContent = questionBank[level].question
-for (let i = 0; i < questionBank[level].selection.length; i++) {
-  const element = questionBank[level].selection[i];
-  var btn = document.createElement("button");
-  btn.textContent = element 
-  btn.onclick = answer;
-  document.getElementById("answer").append(btn)
+  for (let i = 0; i < questionBank[level].selection.length; i++) {
+    const element = questionBank[level].selection[i];
+    var btn = document.createElement("button");
+    btn.textContent = element
+    btn.onclick = answer;
+    document.getElementById("answer").append(btn)
+  }
 }
 
-}
 
 function answer() {
   console.log("button click");
 }
 
+function evaluateAnswer(event) {
+  if (event === questionBank[level].answer) {
+    evaluateAnswer.textContent = "Correct!";
+  } else {
+    timeLeft -= 10;
+    evaluateAnswer.textContent = "Incorrect!";
+  }
+  score = timeLeft;
+  level++;
+  displayQuestion();
+}
+
+
+
+// get the value of the answer which was selected. (Very important)
+// user the selected answer to check if it is the correct answer
+// if it is the correct answer (Compare the selected answer with a value in questionBank in your program because it has the value of the correct answers)
+    // decrease the timer by 10
+// if it is not the correct answer
+    // the timer does not decrease and no further action is required
